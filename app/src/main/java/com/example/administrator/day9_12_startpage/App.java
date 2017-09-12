e1365dad6bb203a03c17d0b7e8c53051d587ad15
gitDemo <3396899309@qq.com>
2017-09-12 21:10:16 +0800

    添加内容

---
+++ app/src/main/java/com/example/administrator/day9_12_startpage/App.java
@@ -0,0 +1,14 @@
+package com.example.administrator.day9_12_startpage;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.umeng.socialize.PlatformConfig;
+
+public class App extends Application {
+    {
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
+    }
+}
